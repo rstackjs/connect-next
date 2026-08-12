6929396a93da3be46132655847556a96fce8a869
neverland <chenjiahan.jait@bytedance.com>
2026-08-12 18:44:07 +0800

    chore(deps): upgrade Rslint to v0.8.0

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig, ts } from '@rslint/core';
+import { defineConfig, js, ts } from '@rslint/core';
 
 export default defineConfig([
+  js.configs.recommended,
   ts.configs.recommended,
+  {
+    files: ['test/**/*'],
+    rules: {
+      '@typescript-eslint/no-this-alias': 'off',
+      'no-undef': 'off',
+    },
+  },
 ]);
